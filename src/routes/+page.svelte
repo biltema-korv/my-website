fc0acb6959196e4d770d48582df073c817976f59
biltema-korv <colbjo@edu.umea.se>
2025-11-03 09:27:18 +0100

    Very good

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,17 @@
 <!--Delar upp sidan i två delar-->
 <main>
   <div>
-
     <h1 class="name"> Förnamn Efternamn </h1>
     <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
     <h3>Inspirerade citat för min webbutvecklingsresa:</h3>
     <!--Under denna kommer vi lägga till fler element-->
+  <p id="inspo">
+    "The only way to learn a new programming language is by writing programs in it." - <strong>Dennis Ritchie</strong>, creator of the C programming language.
+  </p>
+
+  <p style="background-color:pink">
+    "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it." - <span>Patrick McKenzie, software engineer and entrepreneur</span>.
+  </p>
 
   </div>
     <!--Här kommer vi lägga till en bild senare i uppgiften 1.2.4 Extra styling-->  
@@ -14,6 +20,14 @@
 </main>\
 
 <style>
+  #inspo{
+    font-family: courier;
+    font-size: 160%;
+  }
+
+  span{
+      color: green;
+  }
   main{
     /*Styling för att dela upp sidan i två delar. */
     /*Vi kommer gå igenom detta i detalj senare*/
@@ -35,10 +49,6 @@
         font-size: 50px;
     }
     
-
-  
-
-
     h1{
         color:grey;
     }
